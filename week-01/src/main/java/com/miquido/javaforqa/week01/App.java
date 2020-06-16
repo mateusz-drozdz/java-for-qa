513fd0dc28ff668cb62363e2cb8b5fd137148f05
Mateusz Drożdż <mateusz.drozdz@miquido.com>
2020-06-16 11:17:11 +0200

    week-01: project setup

---
+++ week-01/src/main/java/com/miquido/javaforqa/week01/App.java
@@ -0,0 +1,8 @@
+package com.miquido.javaforqa.week01;
+
+public class App {
+  public static void main(String[] args) {
+    System.out.println("Hello");
+  }
+}
+
---
+++ week-01/src/test/java/com/miquido/javaforqa/week01/AppTest.java
@@ -0,0 +1,13 @@
+package com.miquido.javaforqa.week01;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+  @Test
+  void dummyTest() {
+    App subject = new App();
+    assertNotNull(subject);
+  }
+}
